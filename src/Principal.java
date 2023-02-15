4dc8759e4d8b072c3845b0be457b5ff45c416a7c
antonio <bc991977@ens.uqam.ca>
2023-02-15 16:16:42 -0500

    fin implementation boucle d'arret en fonction du nombre d'echecs

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,11 @@
 package src;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 import src.Exceptions.CantAddPieceInEntreSouterraineException;
 import src.Placages.Input.EntreSouterraineFromFileService;
@@ -11,81 +15,63 @@ import src.Placages.Input.PieceServiceInterface;
 import src.Service.PlacagePieceService;
 
 public class Principal {
+    public static  String nomDuFichier;
+    
 
 
     public static void main(String[] args) {
-
+         int compteurEchec = 0;
+        System.out.println("veillez saisir le nom du fichier");
+        Scanner scanner = new Scanner(System.in);
+           nomDuFichier = scanner.nextLine();  
+         EntreSouterraine entreSouterraine = null ;   
+        
         var random = new Random();
         var placagePieceService = new PlacagePieceService(random);
-        // var entreSouterraine = new EntreSouterraine(50, 30, random);
-        // var entreSouterraine = new EntreSouterraine(
-        //     10, 
-        //     15, 
-        //     random, 
-        //     placagePieceService
-        // );
+        PieceServiceInterface pieceService = null;
+        EntreSouterraineServiceInterface entreSouterraineService = null;
+        try{
+            var file = new File(nomDuFichier);
+            entreSouterraineService = new EntreSouterraineFromFileService(
+                file,
+                placagePieceService,
+                random
+            );
+         pieceService = new PieceServiceFromFile(file);
 
-        // entreSouterraine.ajouterPieceCentrale(new Piece(2, 3));
-        // System.out.println(entreSouterraine);
-
-        // entreSouterraine.ajouterPiece(new Piece(4, 5));
-        // System.out.println(entreSouterraine);
-
-        // entreSouterraine.ajouterPiece(new Piece(3, 4));
-        // System.out.println(entreSouterraine);
-
-        // entreSouterraine.ajouterPiece(new Piece(4, 2));
-        // System.out.println(entreSouterraine);
-
-        // entreSouterraine.ajouterPiece(new Piece(3, 5));
-        // System.out.println(entreSouterraine);
-
-        // entreSouterraine.ajouterPiece(new Piece(5, 3));
-        // System.out.println(entreSouterraine);
-
-
-        // var randomGenerator  = new Random();
-        var file = new File("test1.txt");
-        EntreSouterraineServiceInterface entreSouterraineService = new EntreSouterraineFromFileService(
-            file,
-            placagePieceService,
-            random
-        );
-        PieceServiceInterface pieceService = new PieceServiceFromFile(file);
-
-        var entreSouterraine = entreSouterraineService.getEntreSouterraine();
-        // System.out.println(entreSouterraine);
- 
-        entreSouterraine.ajouterPieceCentrale(pieceService.getPiecePrincipale());
-
-        var pieces = pieceService.getPieces();
-        for (int i = 0; i < pieces.size(); i++) {
-            if (i != 0) {
-                try {
-                    entreSouterraine.ajouterPiece(pieces.get(i));
-
-                } catch (CantAddPieceInEntreSouterraineException e) {
-                    System.out.println("La piece " + pieces.get(i) + " ne peut pas etre ajoutee");
-                    // TODO: handle exception
-                }
-            }
+        } catch(RuntimeException e) {
+            System.out.println(" ce fichier n'exsite pas ");
+            return;
         }
 
-        System.out.println(entreSouterraine);
-        // for (int i = 0; i < 2; i++) {
-        //     try {
-        //         // System.out.println(4 - i);
-        //         // entreSouterraine.ajouterPiece(new Piece(1, 1));
-        //         entreSouterraine.ajouterPiece(new Piece( random.nextInt(12) + 1, random.nextInt(12) + 1));
-        //     } catch (CantAddPieceInEntreSouterraineException e) {
-        //         System.out.println("Exception : " + e);
-        //     } catch (ArrayIndexOutOfBoundsException e) {
-        //         System.out.println("La valeur de i lors de l'exception est : " + i);
-        //         throw e;
-        //     }
-        // }
-        // System.out.println(entreSouterraine);
+        try {
+            entreSouterraine = entreSouterraineService.getEntreSouterraine();
+            entreSouterraine.ajouterPieceCentrale(pieceService.getPiecePrincipale()); 
+        } catch (CantAddPieceInEntreSouterraineException e) {
+            System.out.println(entreSouterraine);
+            return;
+        }
+        
 
-        // Cote.getCoteHasard();
-    }
+        var pieces = pieceService.getPieces();
+        pieces = new ArrayList<>(pieces);
+        pieces.remove(0);
+        int i = 0 ;
+        Piece randomPiece = null ;
+        while (i < 100 && !pieces.isEmpty()) {
+            int randomIndex = random.nextInt(pieces.size());
+            randomPiece = pieces.get(randomIndex);
+            try {
+                entreSouterraine.ajouterPiece(randomPiece);
+                pieces.remove(randomPiece);
+            } catch (CantAddPieceInEntreSouterraineException e) {
+                i++;
+            }
+        }
+        
+
+        System.out.println(entreSouterraine);
+        
+
+   }
 }
